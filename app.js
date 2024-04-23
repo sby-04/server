13a5c51fb57909257380f76f621da8658f897909
GanangPrakoso <ganangmanggolo@gmail.com>
2024-04-23 13:41:52 +0700

    fitur register done

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.post("/login", (req, res) => {
   console.log("angggap saja ini feature login");
   res.status(200).json({ access_token: "eyoashdfponsaiu0" });
 });
 
-app.listen(3000, () => console.log("running on port 3000"));
+app.post("/register", (req, res) => {
+  console.log("anggap saja ini feature register dar der dor");
+  res.status(201).json({ msg: "register berhasil" });
+});
+
+app.listen(PORT, () => console.log("haihu " + PORT));
